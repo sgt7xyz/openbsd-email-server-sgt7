9161e32d0201e669107b361a2c1e1933917aa930
sgt7xyz <165677047+sgt7xyz@users.noreply.github.com>
2024-04-19 00:04:43 +0000

    Updated scripts

--- preInstallMail.sh
+++ preInstallMail.sh
@@ -1,14 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env ksh
 
 #Install and configure a Mail Server on OpenBSD. Curently tested
-#on OpenBSD 6.8. I use this script as a "Startup Script" when
+#on OpenBSD 7.5. I use this script as a "Startup Script" when
 #intially installing the server, but you can manually run it if you wish.
 
 #Patch new server install
 pkg_add -u
 
 #Install vim with python3 support
-pkg_add vim
+pkg_add vim-9.1.139-no_x11-python3
+
+echo '' >> ~/.profile
+echo '#Added by preInstallMail.sh' >> ~/.profile
+echo "alias vi='vim'" >> ~/.profile
 
 syspatch
 
