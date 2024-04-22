bc90c24b187fdb38c489c779d0b8c87e59f459f3
sgt7xyz <165677047+sgt7xyz@users.noreply.github.com>
2024-04-22 09:30:00 -0400

    Added prepConfiguration.sh

---
+++ prepConfiguration.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env ksh
+
+# This preps the scripts and configuration files for your domain name.
+# Simply run this script with your new domain name as an argument
+
+# Check if domain name is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 new-domain-name"
+    exit 1
+fi
+
+newDomainName=$1
+
+# Use sed to replace 'DOMAIN-NAME' with the new domain name
+find . -type f -exec sed -i '' "s/DOMAIN-NAME/${newDomainName}/g" {} \;
